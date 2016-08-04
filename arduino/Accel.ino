22d004bf70a6b9a8294f7f00e24cd421ca77807d
kainbozzetto <kainbozzetto@gmail.com>
2016-08-04 14:34:35 +1000

    Add files via upload

---
+++ arduino/Accel.ino
@@ -0,0 +1,23 @@
+int xAxis = A0;
+int yAxis = A1;
+int zAxis = A2;
+int axisValue = 0;
+
+void setup() {
+  Serial.begin(38400);
+}
+
+void loop() {
+  axisValue = analogRead(xAxis);    
+  Serial.print(axisValue);
+  
+  axisValue = analogRead(yAxis);
+  Serial.print(",");
+  Serial.print(axisValue);
+  
+  axisValue = analogRead(zAxis);
+  Serial.print(",");
+  Serial.println(axisValue);
+  
+  delay(200);                 
+}
